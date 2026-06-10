7a7aa3c5cdd3b1f5c844cc49ded703ec60135255
Aaron <braydenaaron123@gmail.com>
2026-06-10 22:26:03 +0800

    Added get_date and save_expenses will be making the main menu after..

--- ExpenseTrack.py
+++ ExpenseTrack.py
@@ -2,11 +2,29 @@ import json
 import os
 import datetime 
 
+today = datetime.today().strftime("%Y-%m-%d")
+
 def load_expenses():
-    print()
+    if os.path.exists("expenses.json"):
+        with open("expenses.json") as file:
+            return json.load(file)
+    return []
     
 def save_expenses(expenses):
-    print()
+    with open("expenses.json", "w") as file:
+        json.dump(expenses, file, indent=2)
+        
+def get_date():
+    date = input("Please input the date in YYYY-MM-DD format (Press enter for today): ")
+    if date == "":
+        return today
+    
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+        return date
+    except ValueError:
+        print("Invalid date formatting, using todays date instead...")
+        return today
 
 def main():
     print()
