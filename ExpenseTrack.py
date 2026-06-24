0247000e01833306aa685da7b9b8ab0fb1950077
Aaron <braydenaaron123@gmail.com>
2026-06-25 00:01:01 +0800

    final commit

--- ExpenseTrack.py
+++ ExpenseTrack.py
@@ -33,131 +33,18 @@ def save_expenses(expenses):
 def get_date():
     today = datetime.today().strftime("%Y-%m-%d")
     
-    date = input("Please input the date in YYYY-MM-DD format (Press enter for today): ")
-    if date == "":
-        return today
-    
-    try:
-        datetime.strptime(date, "%Y-%m-%d")
-        return date
-    except ValueError:
-        print("Invalid date formatting, using todays date instead...")
-        return today
-
-def filtered_expenses(expenses):  
-    print("\nCategories:")
-            
-    for i, cat in enumerate(CATEGORIES, 1):
-        print(f"{i}. {cat}")
-                
     while True:
+        date = input("Please input the date in YYYY-MM-DD format (Press enter for today): ")
+        if date == "":
+            return today
+    
         try:
-            filter_category = int(input("Please select which category you want to filter by(1-6):")) 
-                              
-            if 1 <= filter_category <= 6:
-                print(f"\n--- {CATEGORIES[filter_category - 1]} expenses ---")  
-                print(f"{'ID':<4} {'Date':<12} {'Amount':<10} {'Description'}")
-                        
-                found = False
-                for dictionary in expenses:
-                    if dictionary['category'] == CATEGORIES[filter_category - 1]:
-                            found = True
-                            print(f"{dictionary['id']:<4} {dictionary['date']:<12} {dictionary['amount']:<10.2f} {dictionary['description']} ")
-                if not found:
-                    print(f"No expenses found in {CATEGORIES[filter_category - 1]}")
-                input("\nPress Enter to continue...")
-                break  
-            else:         
-                print("please input a number from 1 to 6")
-                    
+            datetime.strptime(date, "%Y-%m-%d")
+            return date
         except ValueError:
-            print("Please type in a number")
-
-def delete_expense(expenses):
-    if not expenses:
-        print("\nNo expenses to delete")
-    else:
-        page = 1
-        loop = 0
-        total_page = math.ceil(len(expenses)/10)
-        delete_condition = False
-                
-        for i, expense in enumerate(expenses,1):
-            if loop == 0:
-                start_num = (page - 1) * 10 + 1
-                end_num = min(page * 10, len(expenses))
-                print(f"\n--- Current Expenses (Page {page}/{total_page}) ---")
-                print(f"{'ID':<4} {'Date':<12} {'Amount':<10} {'Description'}")
-                        
-            print(f"{expense['id']:<4} {expense['date']:<12} {expense['amount']:<10.2f} {expense['description']} ")
-            loop += 1
-                    
-            if (i % 10) == 0 and i != len(expenses):
-                user_input = input("\nPress Enter for next page, or 'd' to select an expense to delete: ")
-                if user_input.lower() == 'd':
-                    delete_condition = True
-                    break
-                else:
-                    page += 1
-                    loop = 0
-                            
-        if not delete_condition and expenses:
-            user_input = input("\nPress 'd' to delete an expense, or Enter to continue: ")
-            if user_input.lower() == 'd':
-                delete_condition = True
-                            
-        if delete_condition:
-            while True:
-                try:
-                    id_delete = int(input("\nEnter ID to delete (or 0 to cancel): "))
-                    if id_delete == 0:
-                        print("Operation cancelled")
-                        break
-                    
-                    found = False
-                    for idx, exp in enumerate(expenses):
-                        if exp["id"] == id_delete:
-                            deleted_amount = exp["amount"]
-                            deleted_category = exp["category"]
-                            expenses.pop(idx)
-                            save_expenses(expenses)
-                            print(f"\n✅ Deleted: ${deleted_amount} for {deleted_category}")
-                            found = True
-                            break
-                    
-                    if not found:
-                        print("ID not found...")
-                        continue
-                    break
-                    
-                except ValueError:
-                    print("Only integers are allowed")
-                                
-        input("\nPress Enter to continue...")
-                 
-def paginated_expenses(expenses):
-    page = 1
-    loop = 0
-    total_page = math.ceil(len(expenses)/10)
-                    
-    for i, expense in enumerate(expenses,1):
-        if loop == 0:
-            start_num = (page - 1) * 10 + 1
-            end_num = min(page * 10, len(expenses))
-            print(f"\nPage {page}/{total_page} expenses ({start_num}-{end_num}):")
-            print(f"{'ID':<4} {'Date':<12} {'Amount':<10} {'Description'}")
-                        
-        print(f"{expense['id']:<4} {expense['date']:<12} {expense['amount']:<10.2f} {expense['description']} ")
-        loop += 1
-                    
-        if (i % 10) == 0 and i != len(expenses):
-            user_input = input("\nPress Enter for next page, or 'q' to exit: ")
-            if user_input.lower() == 'q':
-                break
-            else:
-                page += 1
-                loop = 0
-
+            print("Invalid date formatting, please try again...\n")
+            continue
+              
 def add_expenses(expenses):
     print("\nLets add expenses")
             
@@ -165,12 +52,11 @@ def add_expenses(expenses):
         try:
             amount = float(input("Enter amount(HK$): "))
             if amount <= 0:
-                print("Amount must be more than 0")
+                print("Amount must be more than 0\n")
                 continue
-            else:
-                break
         except ValueError:
-            print("Enter a valid number...")
+            print("Enter a valid number...\n")
+            continue
             
         print("\nCategories:")
         for i, cat in enumerate(CATEGORIES, 1):
@@ -203,7 +89,36 @@ def add_expenses(expenses):
             
         print(f"Added {amount}HK$ for {category} on {date}")
         input("\nPress Enter to continue...")
+        break
 
+def paginated_expenses(expenses):
+    page = 1
+    loop = 0
+    total_page = math.ceil(len(expenses)/10)
+                    
+    for i, expense in enumerate(expenses,1):
+        if loop == 0:
+            start_num = (page - 1) * 10 + 1
+            end_num = min(page * 10, len(expenses))
+            print(f"\nPage {page}/{total_page} expenses ({start_num}-{end_num}):")
+            print(f"{'ID':<4} {'Date':<12} {'Amount':<10} {'Description'}")
+                        
+        print(f"{expense['id']:<4} {expense['date']:<12} {expense['amount']:<10.2f} {expense['description']} ")
+        loop += 1
+                    
+        if (i % 10) == 0 and i != len(expenses):
+            while True:
+                user_input = input("\nPress Enter for next page, or 'q' to exit: ")
+                if user_input.lower() == 'q':
+                    return
+                elif user_input == "":
+                    page += 1
+                    loop = 0
+                    break
+                else:
+                    print("Please either press enter or type 'd'...")
+                    continue
+                
 def total_spent(expenses):
     if not expenses:
         print("\n No expenses yet")
@@ -229,6 +144,112 @@ def total_spent(expenses):
                 
         input("\nPress Enter to continue...")
 
+def filtered_expenses(expenses):  
+    print("\nCategories:")
+            
+    for i, cat in enumerate(CATEGORIES, 1):
+        print(f"{i}. {cat}")
+                
+    while True:
+        try:
+            filter_category = int(input("Please select which category you want to filter by(1-6): ")) 
+                              
+            if 1 <= filter_category <= 6:
+                print(f"\n--- {CATEGORIES[filter_category - 1]} expenses ---")  
+                print(f"{'ID':<4} {'Date':<12} {'Amount':<10} {'Description'}")
+                        
+                found = False
+                for dictionary in expenses:
+                    if dictionary['category'] == CATEGORIES[filter_category - 1]:
+                            found = True
+                            print(f"{dictionary['id']:<4} {dictionary['date']:<12} {dictionary['amount']:<10.2f} {dictionary['description']} ")
+                if not found:
+                    print(f"No expenses found in {CATEGORIES[filter_category - 1]}")
+                input("\nPress Enter to continue...")
+                break  
+            else:         
+                print("please input a number from 1 to 6\n")
+                    
+        except ValueError:
+            print("Please type in a number\n")
+
+def delete_expense(expenses):
+    if not expenses:
+        print("\nNo expenses to delete")
+    else:
+        page = 1
+        loop = 0
+        total_page = math.ceil(len(expenses)/10)
+        delete_condition = False
+                
+        for i, expense in enumerate(expenses,1):
+            if loop == 0:
+                start_num = (page - 1) * 10 + 1
+                end_num = min(page * 10, len(expenses))
+                print(f"\n--- Current Expenses (Page {page}/{total_page}) ---")
+                print(f"{'ID':<4} {'Date':<12} {'Amount':<10} {'Description'}")
+                        
+            print(f"{expense['id']:<4} {expense['date']:<12} {expense['amount']:<10.2f} {expense['description']} ")
+            loop += 1
+                    
+            if (i % 10) == 0 and i != len(expenses):
+                while True:
+                    user_input = input("\nPress Enter for next page, or 'd' to select an expense to delete: ")
+                    if user_input.lower() == 'd':
+                        delete_condition = True
+                        break
+                    elif user_input == "":
+                        page += 1
+                        loop = 0
+                        break
+                    else:
+                        print("Please either press enter or type 'd'...")
+                        continue
+                if delete_condition:
+                    break
+                    
+                            
+        if not delete_condition and expenses:
+            while True:
+                user_input = input("\nPress 'd' to delete an expense, or Enter to return to main menu: ")
+                if user_input.lower() == 'd':
+                    delete_condition = True
+                    break
+                elif user_input == "":
+                    break
+                else:
+                    print("Please either press enter or type 'd'...")
+                            
+        if delete_condition:
+            while True:
+                try:
+                    id_delete = int(input("Enter ID to delete (or 0 to cancel): "))
+                    if id_delete == 0:
+                        print("Operation cancelled")
+                        break
+                    
+                    found = False
+                    for idx, exp in enumerate(expenses):
+                        if exp["id"] == id_delete:
+                            deleted_amount = exp["amount"]
+                            deleted_category = exp["category"]
+                            expenses.pop(idx)
+                            save_expenses(expenses)
+                            print(f"✅ Deleted: ${deleted_amount} for {deleted_category}")
+                            found = True
+                            break
+                    
+                    if not found:
+                        print("ID not found...")
+                        continue
+                    break
+                    
+                except ValueError:
+                    print("Only integers are allowed")
+        
+        if delete_condition:                   
+            input("\nPress Enter to continue...")
+        
 def monthly_summary(expenses):
      while True:
         try:
@@ -261,7 +282,7 @@ def monthly_summary(expenses):
                     if expense["category"] in categories:
                         categories[expense["category"]] += expense["amount"]
                         total += expense["amount"]
-                    
+            
             print(f"\n{'='*40}")
             print(f"Monthly Summary: {month}")
             print(f"{'='*40}")
@@ -314,6 +335,7 @@ def main():
             break   
         
         else:
-            print("Input a number from 1 to 7")    
+            print("Input a number from 1 to 7")   
+            input("Press enter to continue...") 
                               
 main()
